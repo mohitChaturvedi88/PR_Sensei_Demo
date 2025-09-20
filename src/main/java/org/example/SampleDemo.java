56ac77ecb7e4d2f25e1fc63148d1c2e9cdf0a029
Shivangi Bhadouria <shivangibhadouria@Shivangis-MacBook-Air.local>
2025-09-20 13:17:25 +0530

    Initial commit

---
+++ src/main/java/org/example/SampleDemo.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class SampleDemo {
+
+    // ✅ Simple main method
+    public static void main(String[] args) {
+       System.out.println("Hello World!");
+    }
+
+}
