84cc5857e02adb50ab35aa6363ba3323d50506d4
Shivangi Bhadouria <shivangibhadouria@Shivangis-MacBook-Air.local>
2025-09-20 13:27:46 +0530

    added text

--- src/main/java/org/example/SampleDemo.java
+++ src/main/java/org/example/SampleDemo.java
@@ -5,6 +5,9 @@ public class SampleDemo {
     // ✅ Simple main method
     public static void main(String[] args) {
        System.out.println("Hello World! it's AI world Now");
+       sdfsd
+               sdf
+               sf
     }
 
 }
